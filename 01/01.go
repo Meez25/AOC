1682ac7d2cf04365506233b73dbff158b9e9daa8
Yann BRONNER <yann.bronner@gmail.com>
2024-05-27 22:34:24 +0200

    rename file

--- 01/01.go
+++ 01/01.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"io/fs"
+)
+
+type File struct {
+	content []byte
+}
+
+func NewFileFromFS(fileSystem fs.FS) ([]File, error) {
+	dir, _ := fs.ReadDir(fileSystem, ".")
+	var files []File
+	for range dir {
+		files = append(files, File{})
+	}
+	return files, nil
+}
+
 func main() {
 }
--- 01/01_test.go
+++ 01/01_test.go
@@ -1,12 +1,12 @@
-package main
+package main_test
 
 import (
-	file "github.com/Meez25/AOC"
+	File "github.com/Meez25/AOC/01"
 	"testing"
 	"testing/fstest"
 )
 
-func TestSolution(t *testing.T) {
+func TestFile(t *testing.T) {
 	fs := fstest.MapFS{
 		"input.txt": {Data: []byte(`1abc2
 pqr3stu8vwx
@@ -14,9 +14,15 @@ a1b2c3d4e5f
 treb7uchet`)},
 	}
 
-	decodedFile := file.NewInputFromFS(fs)
+	decodedFile, err := File.NewFileFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(decodedFile) != len(fs) {
 		t.Errorf("got %d lines, wanted %d lines", len(decodedFile), len(fs))
 	}
+
+	got := File{decodedFile[0]}
 }
